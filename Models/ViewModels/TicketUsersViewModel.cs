7df635845bea914e0d04e75e7dcd9894a3c29ea9
dev-vader091 <86165493+dev-vader091@users.noreply.github.com>
2023-03-10 19:56:33 -0600

    added new models

---
+++ Models/ViewModels/TicketUsersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BugHunterBugTrackerZD.Models.ViewModels
+{
+    public class TicketUsersViewModel
+    {
+        public Ticket? Ticket { get; set; }
+        public SelectList? TicketUsers { get; set; }
+
+        public string? SelectedDeveloperId { get; set; }
+
+        //public BTUser? Developer { get; set; }
+    }
+}
